deb022587658b2c38c28affcc697f5e75889a6f6
atharva-9423 <phatangareatharva@gmail.com>
2025-07-01 10:36:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -386,7 +386,7 @@ class AIChatBot {
                 signal: controller.signal,
                 headers: {
                     'Content-Type': 'application/json',
-                    'Authorization': 'Bearer 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'
+                    'Authorization': 'Bearer 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'
                 },
                 body: JSON.stringify({
                     model: 'llama-3.1-8b-instant',
